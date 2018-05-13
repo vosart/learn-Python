0124755cc93e845457ea44894ebe311f6bfb68f7
vosart <metravs@gmail.com>
2018-05-14 01:07:41 +0300

    Project 01 - Done!

--- 01_Higher_Lower/hl.py
+++ 01_Higher_Lower/hl.py
@@ -1,10 +1,21 @@
-print('Enter a new binary digit: ')
-b = input()
-i = 0
+# Project 01 Higher Lower
+import random
 
-while b != '':
-    i = i * 2 + int(b[0])
-    b = b[1:]
-    
-print('Decimal: ', i)
+print('Enter a digit 1-10: ')
+result = False
+pc_digit = random.randint(1, 10)
+
+try:
+
+    while result != True:
+        user_digit = int(input())
+        if user_digit > pc_digit:
+            print('less')
+        elif user_digit < pc_digit:
+            print('more')
+        else:
+            print('BINGO!')
+            result = True
+except ValueError:
+    print("It's not a digit")
 input()
